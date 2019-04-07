d8cf0b3661ae25e2f76083d70acfa10e213d442d
saigane5h <motamarrisaiganesh@gmail.com>
2019-04-07 22:05:42 +0530

    Create 4thquestion.c

---
+++ 4thquestion.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+main()
+{
+	int timer=0,i=0,bt=0;
+	int process[4]={1,2,3,4};
+	int arrival[4]={0,5,13,17};
+	int burst_time[4]={20,36,19,42};
+	int burst[4]={20,36,19,42};
+	int process_arrived[4]={1};
+	double priority[4]={0};
+	int complete[4]={0};
+  }
