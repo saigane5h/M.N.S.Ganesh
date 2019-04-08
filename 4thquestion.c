6c90c96f570943ae1aa96fed5498b5fc7471f57e
saigane5h <motamarrisaiganesh@gmail.com>
2019-04-08 22:40:33 +0530

    Update 4thquestion.c

--- 4thquestion.c
+++ 4thquestion.c
@@ -10,4 +10,78 @@ main()
 	int process_arrived[4]={1};
 	double priority[4]={0};
 	int complete[4]={0};
+		while(1)
+	{
+		int k;
+		for(k=0;k<4;k++)
+		{
+			if(timer>=arrival[k] && burst[k]!=(-1)) 
+			{
+				priority[k]=1+((timer-arrival[k])/burst[k]);
+			}
+		}
+		int p,q;
+		for(p=0;p<4;p++)
+		{
+			for(q=0;q<4-1-p;q++)
+			{
+				if(priority[q]<priority[q+1] && process_arrived[q]!=0 && process_arrived[q+1]!=0)
+				{
+					double t1=priority[q];
+					priority[q]=priority[q+1];
+					priority[q+1]=t1;
+					int t2=process_arrived[q];
+					process_arrived[q]=process_arrived[q+1];
+					process_arrived[q+1]=t2;
+				}
+				else if(priority[q]==priority[q+1]  && process_arrived[q]!=0 && process_arrived[q+1]!=0)
+				{
+					if(burst[q]<burst[q+1])
+					{
+						int t=process_arrived[q];
+						process_arrived[q]=process_arrived[q+1];
+						process_arrived[q+1]=t;
+					}
+				}
+			}
+		}
+		int y=process_arrived[0];
+		bt=burst[y-1];
+		do
+		{
+			printf("\ntimer : %d",timer);
+			int x;
+			for(x=0;x<4;x++)
+			{
+				if(timer==arrival[x])
+				{		
+					process_arrived[x]=x+1;
+					printf("\nprocess %d arrived\n",x);
+				}	
+			}
+			bt=bt-1;
+			timer=timer+1;
+			if(bt==0)
+			{
+				printf("\nprocess %d completed\n",y );
+				complete[y-1]=timer;
+				i=i+1;
+				burst[y-1]=-1;
+				//priority[0]=0;
+				for(x=0;x<4;x++)
+				{
+					priority[x]=0;
+					if(process_arrived[x]!=0)
+					{
+						process_arrived[x]=x+1;
+					}
+				}
+				break;
+			}
+		}while(1);
+		if(i==4)
+			break;
+
+	}
+
   }
