6635dd063236fbbacbfadf399018037d562acdfd
saigane5h <motamarrisaiganesh@gmail.com>
2019-04-16 15:51:47 +0530

    Update 4thquestion.c

--- 4thquestion.c
+++ 4thquestion.c
@@ -1,87 +1,93 @@
-#include<stdio.h>
-
-main()
+# include<pthread.h>
+# include<stdio.h>
+# include<stdlib.h>
+int process[100],runtime[100],waitingtime[100],turnaroundtime[100],c[100],arrivaltime[100],n,total,temp,sum=0,i,j,pos;
+float priority,avgwaitingtime=0,avgturnaroundtime=0;
+void *input();
+void *sort();
+int main()
 {
-	int timer=0,i=0,bt=0;
-	int process[4]={1,2,3,4};
-	int arrival[4]={0,5,13,17};
-	int burst_time[4]={20,36,19,42};
-	int burst[4]={20,36,19,42};
-	int process_arrived[4]={1};
-	double priority[4]={0};
-	int complete[4]={0};
-		while(1)
-	{
-		int k;
-		for(k=0;k<4;k++)
-		{
-			if(timer>=arrival[k] && burst[k]!=(-1)) 
-			{
-				priority[k]=1+((timer-arrival[k])/burst[k]);
-			}
-		}
-		int p,q;
-		for(p=0;p<4;p++)
-		{
-			for(q=0;q<4-1-p;q++)
-			{
-				if(priority[q]<priority[q+1] && process_arrived[q]!=0 && process_arrived[q+1]!=0)
-				{
-					double t1=priority[q];
-					priority[q]=priority[q+1];
-					priority[q+1]=t1;
-					int t2=process_arrived[q];
-					process_arrived[q]=process_arrived[q+1];
-					process_arrived[q+1]=t2;
-				}
-				else if(priority[q]==priority[q+1]  && process_arrived[q]!=0 && process_arrived[q+1]!=0)
-				{
-					if(burst[q]<burst[q+1])
-					{
-						int t=process_arrived[q];
-						process_arrived[q]=process_arrived[q+1];
-						process_arrived[q+1]=t;
-					}
-				}
-			}
-		}
-		int y=process_arrived[0];
-		bt=burst[y-1];
-		do
-		{
-			printf("\ntimer : %d",timer);
-			int x;
-			for(x=0;x<4;x++)
-			{
-				if(timer==arrival[x])
-				{		
-					process_arrived[x]=x+1;
-					printf("\nprocess %d arrived\n",x);
-				}	
-			}
-			bt=bt-1;
-			timer=timer+1;
-			if(bt==0)
-			{
-				printf("\nprocess %d completed\n",y );
-				complete[y-1]=timer;
-				i=i+1;
-				burst[y-1]=-1;
-				//priority[0]=0;
-				for(x=0;x<4;x++)
-				{
-					priority[x]=0;
-					if(process_arrived[x]!=0)
-					{
-						process_arrived[x]=x+1;
-					}
-				}
-				break;
-			}
-		}while(1);
-		if(i==4)
-			break;
+pthread_t t1,t2,t3;
+pthread_create(&t1,NULL,input,NULL);
+pthread_create(&t2,NULL,sort,NULL);
+pthread_join(t1,NULL);
+pthread_join(t2,NULL);
+return 0;    
+}
 
-	}
+void *input()
+{
+printf("Enter number of process and number should be less than 100:");
+ scanf("%d",&n);
+ for(i=0;i<n;i++)
+ {
+ printf("\nenter burst time of process%d:",i+1);
+ scanf("%d",&runtime[i]);
+ printf("enter arrival time of process%d:",i+1);
+ scanf("%d",&arrivaltime[i]); 
+ c[i]=0;
+ sum+=runtime[i];
+ process[i]=i+1;
+}
 
+sort();
+
+printf("\nprocess\tarrival time\tBurst time\tWaiting time\tpriority  Turnaround time ");
+  for(total=arrivaltime[0];total<sum;)
+  {
+   float r=-1;
+   int k;
+  for(i=0;i<n;i++)
+  {
+
+   if(arrivaltime[i]<=total && c[i]!=1)
+    {
+              priority=1 + (total-arrivaltime[i])/runtime[i];
+              if(r <= priority)
+               {
+                r=priority;
+                 k=i;
+               }
+          
+   }
+  
+   }
+  
+  
+   total+=runtime[k]; 
+    
+   waitingtime[k]=total-arrivaltime[k]-runtime[k];
+   turnaroundtime[k]=total-arrivaltime[k];
+    c[k]=1;
+   avgwaitingtime+=waitingtime[k];
+   avgturnaroundtime+=turnaroundtime[k];
+printf("\n %d\t\t%d\t\t%d\t\t%d\t %.2f\t\t%d",process[k],arrivaltime[k],runtime[k],waitingtime[k],priority,turnaroundtime[k]);
   }
+
+printf("\nAverage waiting time:%.2f\n",avgwaitingtime/n);
+printf("\nAverage turn around time:%.2f\n",avgturnaroundtime/n);
+return 0;
+}
+void *sort()
+{
+for(i=0;i<n;i++)
+{
+pos=i;
+for(j=i+1;j<n;j++)
+{
+if(arrivaltime[j]<arrivaltime[pos])
+   pos=j;
+}
+  temp=arrivaltime[i];
+  arrivaltime[i]=arrivaltime[pos];
+  arrivaltime[pos]=temp;
+
+  temp=runtime[i];
+  runtime[i]=runtime[pos];
+  runtime[pos]=temp;
+
+  temp=process[i];
+  process[i]=process[pos];
+  process[pos]=temp;
+    }
+}
